616115a67f14e8aaac321549432bf728551098f9
ChezyName <sadiqahmedsalahudin@gmail.com>
2026-07-16 18:52:41 -0500

    Init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PodB_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PodB_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
