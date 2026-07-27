2dabbec3fa4b20294af0ba1a0ea89a2dda9e6681
NickChyrklund <fn5268jt@go.minnstate.edu>
2026-07-27 18:05:23 -0500

    SUMMARY - Added Strings.resx (used to store hard-coded string resources for locality, much like strings.xml from our project. This is the maUI way) - Added GroceryItem.cs - Added GroceryCategory.cs (enums for produce, dairy, meat, and bakery items. Will include a map to correlate enum values to related icons when we get them) - Added icons resources for the grocery categories to the Resources > Images folder
    NOTES
    - Some of the .xml files in the Images folder were converted from PNG, as microsoft's fonts store with the icons we've used in class had nothing for dairy / meat categories. I read that converting png to xml is not good practice; may have to get different icons in the future
    - GroceryCategory.cs contains a dictionary for getting icons from each category, but does not yet link to any actual image files. Must determine if we want to use .png or .xml files for pictures

---
+++ Model/GroceryCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PodB_MAUI.Model
+{
+    public enum GroceryCategory
+    {
+        PRODUCE,
+        DAIRY,
+        MEAT,
+        BAKERY
+    }
+
+    public static class StatusAssets
+    {
+        public static readonly Dictionary<GroceryCategory, string> ImagePaths = new()
+    {
+        { GroceryCategory.PRODUCE, "icons/produce.png" },
+        { GroceryCategory.DAIRY, "icons/alert.png" },
+        { GroceryCategory.MEAT, "icons/cross.png" },
+        { GroceryCategory.BAKERY, "icons/check.png" }
+    };
+    }
+}
