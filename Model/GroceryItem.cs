b2f0a7c29d49ee7bc3055ba2466ecac72df42375
ChezyName <sadiqahmedsalahudin@gmail.com>
2026-07-30 18:48:44 -0500

    Updated Grocery Item

--- Model/GroceryItem.cs
+++ Model/GroceryItem.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace PodB_MAUI.Model
 {
-    public record GroceryItem(string name, GroceryCategory category, double price);
+    public record GroceryItem {
+        public string Name { get; init; } = string.Empty;
+        public string Image { get; init; } = string.Empty; //Url for the image
+        public GroceryCategory Category { get; init; }
+        public double Price { get; init; }
+        public int Quantity { get; init; }
+
+        public double TotalPrice => Price * Quantity
+    }
 }
+
+
+// name
+// category
+// price
+// description
+// image_url -> https://path_to_image.jpg
