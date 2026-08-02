5062be682a44bb5df0c7a5d102c21567fc1f8485
ChezyName <286831011+ahmedsadiq04@users.noreply.github.com>
2026-08-01 23:08:59 -0500

    Small Bug Fixes

--- Model/GroceryItem.cs
+++ Model/GroceryItem.cs
@@ -5,19 +5,43 @@ using System.Text;
 
 namespace PodB_MAUI.Model
 {
-    public record GroceryItem {
+    public record GroceryItem
+    {
+        public GroceryItem(string name, GroceryCategory category, double price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+
+        public GroceryItem(string name, GroceryCategory category, double price, string unit)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+            Unit = unit;
+        }
+
         public string Name { get; init; } = string.Empty;
         public string Image { get; init; } = string.Empty; //Url for the image
         public GroceryCategory Category { get; init; }
 
         public double Price { get; init; }
         public int Quantity { get; init; }
-        public string Unit { get; init; } = "Unit(s)"; //$price x quantity $(Unit)
+        public string Unit { get; init; } = "Unit"; //$price x quantity $(Unit)
 
-        public double TotalPrice => Price * Quantity
+        public double TotalPrice => Price * Quantity;
+
+        public static readonly GroceryItem[] DefaultItems =
+        [
+            new("Apples", GroceryCategory.PRODUCE, 2.99),
+            new("Milk", GroceryCategory.DAIRY, 3.49, "Gallon"),
+            new("Whole Wheat Bread", GroceryCategory.BAKERY, 2.50),
+            new("95/5 Lean Ground Beef", GroceryCategory.MEAT, 8.70, "lb"),
+            new("Blueberry Muffin", GroceryCategory.BAKERY, 3.99),
+        ];
     }
 }
-
 
 // name
 // category
